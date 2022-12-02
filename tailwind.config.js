80d258d6360860812bd56658229372def63b01c6
Bisma Surya <bismasuryamahendra@gmail.com>
2022-12-02 13:41:31 +0700

    bismahendra porto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#171717',
+        white: '#FAFAFA',
+        grey: '#CDCDCD',
+      },
+    },
+  },
+  plugins: [],
+}
